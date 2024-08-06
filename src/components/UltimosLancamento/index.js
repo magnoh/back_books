4e7a99f5791140c18d2a748be99436b44f3b7cf0
Magno Henrique Souza Cardoso <magno.cardoso@GMVB.LOCAL>
2024-08-06 11:43:34 -0300

    feat:criando novo card de descrição do livro

--- src/components/UltimosLancamento/index.js
+++ src/components/UltimosLancamento/index.js
@@ -1,21 +1,16 @@
 import { livros } from "./dadosUltimosLancamentos"
+import { Titulo } from "../Titulo"
+import imagemLivro from '../../images/livro2.png'
+import CardRecomenda from '../CardRecomenda'
 import styled from "styled-components"
+
+
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -29,12 +24,25 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return(
         <UltimosLancamentosContainer>
-            <Titulo>ULTIMOS LANCAMENTOS</Titulo>
+            <Titulo 
+                cor="#000"
+                tamanhoFonte="36px"
+                alinhamento="center"
+            >
+                ULTIMOS LANCAMENTOS
+            </Titulo>
             <NovosLivrosContainer>
                 { livros.map( livro => (
                     <img src={livro.src}/>
                 ) ) }
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talves você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Contruindo uma aplicação"
+                img={imagemLivro}
+            />
+
         </UltimosLancamentosContainer>
         
     )
